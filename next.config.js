21752f6834d8a03e74cc660af6b1baeda50203d2
mohammedsafwat2399 <86661297+mohammedsafwat2399@users.noreply.github.com>
2023-05-22 18:28:39 +0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env:{
-MONGO_URI:"mongodb+srv://mohammed2399safwat:3w6zU79WWxIk34Jd@cluster0.1iweub8.mongodb.net/products-app?retryWrites=true&w=majority",
-APP_DEV:"http://localhost:3000",
-APP_PROD:"",
-  },
   images: {
     domains: ["images.pexels.com"],
   },
